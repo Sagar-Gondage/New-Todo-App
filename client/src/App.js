77fea4a1cb9fdddb86416e663e969d346b1e661c
Sagar-Gondage <sagargondage370@gmail.com>
2022-09-13 21:10:16 +0530

    routes created

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,80 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { Route, Routes, Navigate } from "react-router-dom";
+
+const PrivateRoute = ({ isLoggedin, children }) => {
+  return isLoggedin ? children : <Navigate to="/login" />;
+};
+
+const NestedRoute = ({ isLoggedin, children }) => {
+  return (
+    <Route
+      path="/"
+      element={<PrivateRoute isLoggedin={isLoggedin}>{children}</PrivateRoute>}
+    />
+  );
+};
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <div className="body_container">
+        <Routes>
+          <Route path="/signup" element={<p>signup</p>} />
+          <Route path="/login" element={<p>login</p>} />
+          <Routes>
+            <Route path="/signup" element={<Signup />} />
+            <Route path="/login" element={<Login />} />
+            <Route
+              path="/"
+              element={
+                <PrivateRoute isLoggedin={loggedin}>
+                  <Home />
+                </PrivateRoute>
+              }
+            />
+            <Route
+              path="/newtask"
+              element={
+                <PrivateRoute isLoggedin={loggedin}>
+                  <NewTask />
+                </PrivateRoute>
+              }
+            />
+            <Route
+              path="/alltodos"
+              element={
+                <PrivateRoute isLoggedin={loggedin}>
+                  <AllTodos />
+                </PrivateRoute>
+              }
+            />
+            <Route
+              path="/personaltodos"
+              element={
+                <PrivateRoute isLoggedin={loggedin}>
+                  <PersonalTodos />
+                </PrivateRoute>
+              }
+            />
+            <Route
+              path="/officialtodos"
+              element={
+                <PrivateRoute isLoggedin={loggedin}>
+                  <OfficialTodos />
+                </PrivateRoute>
+              }
+            />
+            <Route
+              path="/othertodos"
+              element={
+                <PrivateRoute isLoggedin={loggedin}>
+                  <OtherTodos />
+                </PrivateRoute>
+              }
+            />
+          </Routes>
+        </Routes>
+      </div>
     </div>
   );
 }
